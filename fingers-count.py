8c34ce9a0b8f8d6edb701988017685150d04c98e
kimminje0ng <kimmin9642@dgu.ac.kr>
2022-05-17 19:14:33 +0900

    ADD finger counter module for two hands

--- fingers-count.py
+++ fingers-count.py
@@ -11,24 +11,35 @@ webcamFeed = cv2.VideoCapture(0)
 while True:
     status, image = webcamFeed.read()
     handLandmarks = handDetector.findHandLandMarks(image=image, draw=True)
-    count=0
+    count = 0
 
     if(len(handLandmarks) != 0):
-        #we will get y coordinate of finger-tip and check if it lies above middle landmark of that finger
-        #details: https://google.github.io/mediapipe/solutions/hands
+        if handLandmarks[4][3] == "Right" and handLandmarks[4][1] > handLandmarks[3][1]:    # 오른쪽 엄지(Right Thumb)
+            count = count+1
+        elif handLandmarks[4][3] == "Left" and handLandmarks[4][1] < handLandmarks[3][1]:   # 왼쪽 엄지(Left Thumb)
+            count = count+1
+        if handLandmarks[8][2] < handLandmarks[6][2]:   # 검지(Index finger)
+            count = count+1
+        if handLandmarks[12][2] < handLandmarks[10][2]: # 중지(Middle finger)
+            count = count+1
+        if handLandmarks[16][2] < handLandmarks[14][2]: # 약지(Ring finger)
+            count = count+1
+        if handLandmarks[20][2] < handLandmarks[18][2]: # 새끼(Little finger)
+            count = count+1
 
-        if handLandmarks[4][3] == "Right" and handLandmarks[4][1] > handLandmarks[3][1]:       #Right Thumb
-            count = count+1
-        elif handLandmarks[4][3] == "Left" and handLandmarks[4][1] < handLandmarks[3][1]:       #Left Thumb
-            count = count+1
-        if handLandmarks[8][2] < handLandmarks[6][2]:       #Index finger
-            count = count+1
-        if handLandmarks[12][2] < handLandmarks[10][2]:     #Middle finger
-            count = count+1
-        if handLandmarks[16][2] < handLandmarks[14][2]:     #Ring finger
-            count = count+1
-        if handLandmarks[20][2] < handLandmarks[18][2]:     #Little finger
-            count = count+1
+        if handLandmarks[4][4] == 2:  # 손이 2개 일때 다른 손도 더함
+            if handLandmarks[25][3] == "Right" and handLandmarks[25][1] > handLandmarks[24][1]:    # 오른쪽 엄지(Right Thumb)
+                count = count+1
+            elif handLandmarks[25][3] == "Left" and handLandmarks[25][1] < handLandmarks[24][1]:   # 왼쪽 엄지(Left Thumb)
+                count = count+1
+            if handLandmarks[29][2] < handLandmarks[27][2]:
+                count = count+1
+            if handLandmarks[33][2] < handLandmarks[31][2]:
+                count = count+1
+            if handLandmarks[37][2] < handLandmarks[35][2]:
+                count = count+1
+            if handLandmarks[41][2] < handLandmarks[39][2]:
+                count = count+1
 
     cv2.putText(image, str(count), (45, 375), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 0, 0), 25)
     cv2.imshow("Volume", image)
--- handDetector.py
+++ handDetector.py
@@ -1,46 +1,51 @@
 import mediapipe as mp
 import cv2
 
-#Initializations: static code
+## mediapipe는 hand detect를 먼저하고, 이후에 track을 함.
+
+# mediapipe 모듈 초기화
 mpHands = mp.solutions.hands
 mpDraw = mp.solutions.drawing_utils
 
-
-
 class HandDetector:
-    def __init__(self, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
-        #when the mediapipe is first started, it detects the hands. After that it tries to track the hands
-        #as detecting is more time consuming than tracking. If the tracking confidence goes down than the
-        #specified value then again it switches back to detection
+    def __init__(self, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):  # max_num_hands = 2
         self.hands = mpHands.Hands(max_num_hands=max_num_hands, min_detection_confidence=min_detection_confidence,
                                    min_tracking_confidence=min_tracking_confidence)
 
 
-    def findHandLandMarks(self, image, handNumber=0, draw=False):
+    def findHandLandMarks(self, image, draw=False):
         originalImage = image
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # mediapipe needs RGB
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = self.hands.process(image)
+
         landMarkList = []
+        label = [[], []]
 
-        if results.multi_handedness:
-            label = results.multi_handedness[handNumber].classification[0].label  # label gives if hand is left or right
-            #account for inversion in webcams
-            if label == "Left":
-                label = "Right"
-            elif label == "Right":
-                label = "Left"
+        if results.multi_handedness:            
+            for hand_cnt in range(len(results.multi_handedness)):
+                tmp_label = results.multi_handedness[hand_cnt].classification[0].label  # left/right hand labeling
+                if tmp_label == "Left":  # 웹캠 반대로 labeling
+                    label[hand_cnt] = "Right"
+                elif tmp_label == "Right":
+                    label[hand_cnt] = "Left"
+            
 
+        if results.multi_hand_landmarks:  # 손 검출 안되면, None return
+            hand_flag = True    
+            for hand in results.multi_hand_landmarks:
+                for id, landMark in enumerate(hand.landmark):
+                    imgH, imgW, imgC = originalImage.shape  # imgH: height, imgW: width, imgC: channel
+                    xPos, yPos = int(landMark.x * imgW), int(landMark.y * imgH)
+                    
+                    hand_cnt = len(results.multi_handedness)
+                    if hand_flag == True: 
+                        landMarkList.append([id, xPos, yPos, label[0], hand_cnt])  # [id: 0~20, Xpos, yPos, label: Left, Right, hand_cnt: 0~1]
+                    else:
+                        landMarkList.append([id, xPos, yPos, label[1], hand_cnt])
+                    if id >= 20:  # 양손 따로 체크
+                        hand_flag = False
 
-        if results.multi_hand_landmarks:  # returns None if hand is not found
-            hand = results.multi_hand_landmarks[handNumber] #results.multi_hand_landmarks returns landMarks for all the hands
-
-            for id, landMark in enumerate(hand.landmark):
-                # landMark holds x,y,z ratios of single landmark
-                imgH, imgW, imgC = originalImage.shape  # height, width, channel for image
-                xPos, yPos = int(landMark.x * imgW), int(landMark.y * imgH)
-                landMarkList.append([id, xPos, yPos, label])
-
-            if draw:
-                mpDraw.draw_landmarks(originalImage, hand, mpHands.HAND_CONNECTIONS)
+                if draw:
+                    mpDraw.draw_landmarks(originalImage, hand, mpHands.HAND_CONNECTIONS)
 
         return landMarkList
